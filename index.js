54f4dbcc3f5082ec90413ccb862b6a6d0577fb8e
riiddhii757 <riddhhi757@gmail.com>
2025-01-05 00:38:25 -0500

    index.js update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import GoogleStrategy from "passport-google-oauth2";
 import session from "express-session";
-import env from "dotenv";
+import dotenv from "dotenv";
 
 const app = express();
 const port = process.env.PORT || 3000; 
