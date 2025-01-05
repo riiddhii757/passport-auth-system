e23e534214ede1a3870b4ec0506d296d7ec9bdbb
riiddhii757 <riddhhi757@gmail.com>
2025-01-05 00:54:50 -0500

    message

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ passport.use(
   "google",
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientId : process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:3000/auth/google/secrets",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
